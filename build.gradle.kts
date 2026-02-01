4e6387d2aaaa5125c5e84819a675b91eeeed2246
29cmb <devcmb@outlook.com>
2026-02-01 10:16:48 -0500

    feat: :tada: Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    kotlin("jvm") version "2.3.20-Beta2"
+    id("com.gradleup.shadow") version "8.3.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+group = "xyz.devcmb"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.29cmb.InvControl:invcontrol-core:v0.1.6")
+    implementation("team.unnamed:creative-api:1.7.3")
+}
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("1.21.11")
+    }
+}
+
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
---
+++ src/main/kotlin/xyz/devcmb/playground/ParkourPlayground.kt
@@ -0,0 +1,13 @@
+package xyz.devcmb.playground
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ParkourPlayground : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
